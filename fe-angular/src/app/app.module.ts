154bba220acab71ae84d32cb1f6a68e55dfa8430
l-s-b <lucio_sb@yahoo.com.ar>
2023-08-06 23:12:04 -0300

    v0.3.1 - Dumb Edit Note page (just UI)

--- fe-angular/src/app/app.module.ts
+++ fe-angular/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainSpaceComponent } from './pages/main-space/main-space.component';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
+import { EditNoteComponent } from './pages/edit-note/edit-note.component';
+import { NoteDetailComponent } from './pages/note-detail/note-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainSpaceComponent,
+    EditNoteComponent,
+    NoteDetailComponent,
   ],
   imports: [
     BrowserModule,
