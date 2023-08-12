d4fd891b1439fa46ff61937126182a5f26a5dd68
l-s-b <lucio_sb@yahoo.com.ar>
2023-08-12 16:53:29 -0300

    v1.1.1 - Swagger fixes

--- be-nest/src/notes/notes.controller.ts
+++ be-nest/src/notes/notes.controller.ts
@@ -14,20 +14,21 @@ import { NotePatch } from './NotePatch.entity';
 import { NotesService } from './notes.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiParam } from '@nestjs/swagger/dist';
 
 @ApiTags('Notes')
 @Controller('notes')
 export class NotesController {
   constructor(private notesService: NotesService) {}
 
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.findOne(id);
+  }
+
   @Get()
   findAll() {
     return this.notesService.getNotes();
-  }
-
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id) {
-    return this.notesService.findOne(id);
   }
 
   @ApiBearerAuth()
